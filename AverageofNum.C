b3dbdce1e9b7d2185734e2643e36ef4b9fba1ecf
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-04 21:48:46 +0530

    Add files via upload

---
+++ AverageofNum.C
@@ -0,0 +1,30 @@
+// Program to print average of the numbers:
+
+// Following program can be used to get the average of the 3 numbers.
+
+#include<stdio.h>
+
+int main(){
+ 
+   
+       
+              int a,b,c;
+ 
+   
+           printf("Enter number A");
+ 
+              scanf("%d",&a);
+    
+              printf("Enter number B");
+ 
+              scanf("%d",&b);
+  
+              printf("Enter number C");
+ 
+              scanf("%d",&c);
+  
+              printf("%d",(a+b+c)/3);
+
+
+return 0;
+}
